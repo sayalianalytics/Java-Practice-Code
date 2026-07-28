4a2655e5daf0a0c52ba482f62cc1a1be0b9f6358
Sayali <sayalichaudhari420@gmail.com>
2026-07-28 22:23:21 +0530

    changes modified

--- src/Freamework_eg.java
+++ src/Freamework_eg.java
@@ -17,7 +17,6 @@ public class Freamework_eg{
 
          System.out.println(list.contains("Sayali"));
 
-             Collections.reverse(list);     
-            System.out.println(list);
+            
     }
 }
