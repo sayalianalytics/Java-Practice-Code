2632e31020397ca98084989bb572f595358bf0c5
Sayali <sayalichaudhari420@gmail.com>
2026-07-28 13:13:59 +0530

    Add new one

---
+++ src/Freamework_eg.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Freamework_eg{
+    public static void main(String[] args){
+        
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Rahul");
+        list.add("Sayali");
+        list.add("Amit");
+        list.add("Rahul");
+
+         System.out.println(list);
+         
+         list.remove("Amit");
+         System.out.println(list);
+
+         System.out.println(list.contains("Sayali"));
+
+             Collections.reverse(list);     
+            System.out.println(list);
+    }
+}
